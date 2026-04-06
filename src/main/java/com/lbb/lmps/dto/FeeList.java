7e82a886b4ef71f5c5010a7a00e9ccd0e37dea28
nohder <pisces2531@gmail.com>
2026-04-06 21:10:31 +0700

    inquiry register done but not (image url)

--- src/main/java/com/lbb/lmps/dto/FeeList.java
+++ src/main/java/com/lbb/lmps/dto/FeeList.java
@@ -1,6 +1,7 @@
 package com.lbb.lmps.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -8,7 +9,10 @@ import java.util.List;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FeeList {
+    @JsonProperty("LAK")
     private List<FeeEntry> LAK;
+    @JsonProperty("THB")
     private List<FeeEntry> THB;
+    @JsonProperty("USD")
     private List<FeeEntry> USD;
 }
