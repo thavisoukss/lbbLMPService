ad69a825e83ecda688cf34d72032cd4ef97101f2
anoudeth <pisces2531@gmail.com>
2026-05-05 14:33:56 +0700

    change context path

--- src/main/java/com/lbb/lmps/controller/LmpsController.java
+++ src/main/java/com/lbb/lmps/controller/LmpsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/lmps")
+@RequestMapping("/payment/lmps")
 @RequiredArgsConstructor
 public class LmpsController {
 
--- src/main/java/com/lbb/lmps/controller/P2PController.java
+++ src/main/java/com/lbb/lmps/controller/P2PController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/p2p")
+@RequestMapping("/payment/p2p")
 @RequiredArgsConstructor
 public class P2PController {
 
